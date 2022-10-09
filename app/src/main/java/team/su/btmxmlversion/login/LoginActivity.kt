d7d4a880aafcbdc29b9ca818c252f4d71632c00f
GunHyung Ham <gdreagon@naver.com>
2022-10-09 17:31:08 +0900

    Manifest 앱 시작점 변경, LoginActivity추가 및 패키지 각 액티비티 뷰바인딩 적용

---
+++ app/src/main/java/team/su/btmxmlversion/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package team.su.btmxmlversion.login
+
+import android.os.Bundle
+import team.su.btmxmlversion.config.BaseActivity
+import team.su.btmxmlversion.databinding.ActivityLoginBinding
+
+class LoginActivity: BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
---
+++ app/src/main/java/team/su/btmxmlversion/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package team.su.btmxmlversion.main
+
+import android.os.Bundle
+import team.su.btmxmlversion.config.BaseActivity
+import team.su.btmxmlversion.databinding.ActivityMainBinding
+
+class MainActivity: BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
